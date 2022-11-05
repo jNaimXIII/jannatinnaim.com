359677653f117476c907468da8ff913bc1820b6e
Jannatin Naim <soup@jannatinnaim.com>
2022-11-05 14:27:10 +0600

    reset

--- src/lib/stores/theme.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from "svelte/store";
-
-export type Theme = "dark" | "light" | "none";
-export const theme = writable<Theme>("none");
-
-export const USER_SELECTED_THEME_KEY = "userSelectedTheme";
--- tests/test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("home page has greeting", async ({ page }) => {
-  await page.goto("/");
-
-  expect(await page.textContent("h1")).toBe("Heyo! How's it going?");
-});
