904fe4b5d55481bc6bdc3cb2ed1e24d133c278b7
Jannatin Naim <soup@jannatinnaim.com>
2023-03-02 07:43:25 +0600

    reset

--- src/lib/utils/api/mal.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { MAL_API_URL, MAL_CLIENT_ID } from "$env/static/private";
-
-export const mal = axios.create({
-  baseURL: MAL_API_URL,
-  headers: {
-    "X-MAL-CLIENT-ID": MAL_CLIENT_ID,
-  },
-});
--- tests/test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("home page has greeting", async ({ page }) => {
-  await page.goto("/");
-  expect(await page.textContent("[data-role='greeting']")).toBe("Heyo! How's it going?");
-});
