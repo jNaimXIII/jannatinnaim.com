6e1d1f17f9150aa53851c3d23c9f0d0045cad9d8
Jannatin Naim <soup@jannatinnaim.com>
2023-06-06 19:22:05 +0600

    add background gradient

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -109,3 +109,24 @@
 		</ul>
 	</nav>
 </section>
+
+<style>
+	:global(body) {
+		background: linear-gradient(231deg, #bcabae, #0f0f0f, #2d2e2e, #716969, #fbfbfb);
+		background-size: 1000% 1000%;
+
+		animation: gradientAnimation 30s ease infinite;
+	}
+
+	@keyframes gradientAnimation {
+		0% {
+			background-position: 14% 0%;
+		}
+		50% {
+			background-position: 87% 100%;
+		}
+		100% {
+			background-position: 14% 0%;
+		}
+	}
+</style>
